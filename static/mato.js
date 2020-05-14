355a05a9e3d905463ca432ccf2fb8227cef33d3d
Vincenius <vincet.will@vaitrade.de>
2020-05-14 08:28:09 +0200

    init commit

---
+++ static/mato.js
@@ -0,0 +1,13 @@
+
+var _paq = window._paq || [];
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+var u="//analytics.vincentwill.de/";
+_paq.push(['setTrackerUrl', u+'hokuspokusp']);
+_paq.push(['setSiteId', '5']);
+var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'hokuspokusj'; s.parentNode.insertBefore(g,s);
+})();
+
